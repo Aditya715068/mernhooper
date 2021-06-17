265e888dbfa88da992faba9efa8455d28c9967e1
Aditya715068 <78248202+Aditya715068@users.noreply.github.com>
2021-06-17 18:50:49 +0530

    Update conn.js

--- server/conn.js
+++ server/conn.js
@@ -1,6 +1,6 @@
 const mongoose= require('mongoose');
 
-const DB= 'mongodb+srv://mernhooper:ADITYA@11111@cluster0.nhljj.mongodb.net/mernhopper?retryWrites=true&w=majority'
+const DB= 'mongodb+srv://mernhooper:<password>@cluster0.nhljj.mongodb.net/mernhopper?retryWrites=true&w=majority'
 
 
 
